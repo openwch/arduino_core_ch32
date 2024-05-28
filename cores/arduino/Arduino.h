7c27b0d5c6a84649580efb7a4b7b037793f01d00
KenjutsuGH <pjakruger@yahoo.com>
2024-05-28 09:45:49 +0200

    Add WCharacter support (#100)
    thanks!

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -49,6 +49,14 @@ void yield(void);
 } // extern "C"
 #endif // __cplusplus
 
+#ifdef __cplusplus
+#  include "WCharacter.h"
+#  include "WString.h"
+#  include "WMath.h"
+//#  include <Tone.h>
+#  include <HardwareSerial.h>
+#endif
+
 // Include pins variant
 #include "pins_arduino.h"
 
