48ccfd26b28620e8ce78577fe638c07c9bfa14dc
Ha Thach <thach@tinyusb.org>
2024-07-04 09:29:53 +0700

    fix hardfault when missing Adafruit_TinyUSB.h header (#131)

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -11,15 +11,21 @@ int main( void )
 {
     pre_init( );
 #if defined(USE_TINYUSB)
-    TinyUSB_Device_Init(0);
+    if (TinyUSB_Device_Init) {
+        TinyUSB_Device_Init(0);
+    }
 #endif
     setup( );
   
     do {
         loop( );
 #if defined(USE_TINYUSB)
-      TinyUSB_Device_Task();
-      TinyUSB_Device_FlushCDC();
+        if (TinyUSB_Device_Task) {
+            TinyUSB_Device_Task();
+        }
+        if (TinyUSB_Device_FlushCDC) {
+            TinyUSB_Device_FlushCDC();
+        }
 #endif
     } while (1);
 
