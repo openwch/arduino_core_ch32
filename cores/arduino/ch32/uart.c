f504b8f0fd0ef950284394d591846e6b81b58e09
Maxint R&D <mm_rd@maxint.nl>
2025-05-06 10:31:38 +0200

    Added uart_putc according PR #180

--- cores/arduino/ch32/uart.c
+++ cores/arduino/ch32/uart.c
@@ -528,6 +528,31 @@ int uart_getc(serial_t *obj, unsigned char *c)
   return 0;
 }
 
+// PR180: HardwareSerial: use correct UART HW for TX
+/**
+  * @brief  Write byte to uart
+  * @param  obj : pointer to serial_t structure
+  * @retval error status
+  */
+int uart_putc(serial_t *obj, unsigned char c)
+{
+  uint32_t tickstart = GetTick();
+
+  if (obj == NULL) {
+    return -1;
+  }
+
+  while (serial_tx_active(obj))
+  {
+    if ((GetTick() - tickstart) >= TX_TIMEOUT)
+    {
+      return 0; // 0 means no error? Should be timeout error?
+    }
+  }
+
+  USART_SendData(uart_handlers[obj->index]->Instance, c);
+  return 0; // 0 means no error
+}
 
 
 
